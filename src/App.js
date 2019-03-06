573ced7ce80d818bbfaa189a4b5ade700f88f3fc
bwielk <bwielk@gmail.com>
2019-03-06 23:01:23 +0000

    Created a state parameter with all the mp3 keys values to be looped through

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ import './App.css';
 import Key from './Components/Key.js';
 
 class App extends Component {
+  state = {
+    combinationsOfKeys: [
+      {'A': [0,7]},
+      {'Ab' : [1,7]},
+      {'B': [0,7]},
+      {'Bb': [0.7]},
+      {'C': [1,8]},
+      {'D': [1,7]},
+      {'Db': [1,8]},
+      {'E': [1,7]},
+      {'Eb': [1,7]},
+      {'F': [1,7]},
+      {'G': [1,7]},
+      {'Gb': [1,7]}
+    ]
+  }
 
   render() {
-    let keys = []
+    const keys = []
+    const words = []
 
+    for(let i=0; i<this.state.combinationsOfKeys.length; i++){
+      words.push(i)
+    };
+
+    console.log(this.state.combinationsOfKeys.length)
     for(let i=0; i<10; i++){
-      keys.push(<Key/>)
+      keys.push(<Key key={i}/>)
     }
+    console.log({keys})
+    console.log({words})
+
     return (
       <div>
         {keys}
